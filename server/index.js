32643f2e87730c1f121a8393115d27e7c91d795f
SantiRosso <santiagorosso@live.com.ar>
2023-02-27 14:46:23 -0300

    first commit, back config

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
+const { DB_PORT } = process.env || "http://localhost:3001";
+
+// Syncing all the models at once.
+conn.sync({ force: false }).then(() => {
+  server.listen(DB_PORT, () => {
+    console.log(`%s listening at ${DB_PORT}`); // eslint-disable-line no-console
+  });
+});
