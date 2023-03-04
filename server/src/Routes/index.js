9c244b1b2847bc52c8df79b478a4e13078ffe0e6
SantiRosso <santiagorosso@live.com.ar>
2023-03-03 22:41:21 -0300

    new route(getGenres)

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 const moviesRouter = require("./Movies/movies.js");
+const genresRouter = require("./Genres/genres.js");
 
 router.use("/movies", moviesRouter);
+router.use("/genres", genresRouter);
 
 module.exports = router;
