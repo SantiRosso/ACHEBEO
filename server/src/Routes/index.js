d1243cd91648d0093987592c64a57c2db03eb7e9
Lucascserrate <lucascserrate@gmail.com>
2023-03-04 20:10:12 -0400

    User funcionando

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require("express");
 const router = Router();
 const moviesRouter = require("./Movies/movies.js");
 const genresRouter = require("./Genres/genres.js");
+const usersRouter = require("./Users/users.routes.js");
 
 router.use("/movies", moviesRouter);
 router.use("/genres", genresRouter);
+router.use("/users", usersRouter);
 
 module.exports = router;
