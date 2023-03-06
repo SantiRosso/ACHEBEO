6c09011e28932675864fa30bb2339aaa4f1fcb1e
SantiRosso <santiagorosso@live.com.ar>
2023-03-06 00:22:51 -0300

    new route (getUserById)

--- server/src/Routes/Users/users.routes.js
+++ server/src/Routes/Users/users.routes.js
@@ -1,25 +1,34 @@
-const { Router } = require('express');
-const User = require('../../models/User');
-const { postUser, getUsers } = require('./controllers')
+const { Router } = require("express");
+const User = require("../../models/User");
+const { postUser, getUsers, getUserById } = require("./controllers");
 
-const router = Router()
+const router = Router();
 
-router.get('/', async (req, res) => {
-    try {
-        res.send(await getUsers())
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-router.post('/', async (req, res) => {
-    const { name, lastname, email, password } = req.body
-    try {
-        await postUser(name, lastname, email, password)
-        res.sendStatus(204)
-    } catch (error) {
-        res.status(400).send(error.message)
-    }
+router.get("/", async (req, res) => {
+  try {
+    res.send(await getUsers());
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-})
+router.get("/", async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.send(await getUserById(id));
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-module.exports = router
+router.post("/", async (req, res) => {
+  const { name, lastname, email, password } = req.body;
+  try {
+    await postUser(name, lastname, email, password);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+module.exports = router;
