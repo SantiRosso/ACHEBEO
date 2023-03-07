36f3aec4660ccd86bd144555b7ef6eaf68e4f0e6
Lucascserrate <lucascserrate@gmail.com>
2023-03-06 22:08:28 -0400

    cambiando los nombres de las rutas

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -1,13 +1,15 @@
 const { Router } = require("express");
 const router = Router();
-const moviesRouter = require("./Movies/movies.routes.js");
-const genresRouter = require("./Genres/genres.routes.js");
-const genresSeries = require("./Series/series.routes.js");
-const usersRouter = require("./Users/users.routes.js");
+const movies = require("./Movies/movies.routes.js");
+const genres = require("./Genres/genres.routes.js");
+const series = require("./Series/series.routes.js");
+const users = require("./Users/users.routes.js");
+const profiles = require("./Users/users.routes.js");
 
-router.use("/movies", moviesRouter);
-router.use("/genres", genresRouter);
-router.use("/users", usersRouter);
-router.use("/series", genresSeries);
+router.use("/movies", movies);
+router.use("/genres", genres);
+router.use("/series", series);
+router.use("/users", users);
+router.use("/profiles", profiles);
 
 module.exports = router;
