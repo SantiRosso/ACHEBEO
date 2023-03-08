b4495d100ad88f674eed5194ab1a1352d8ad89d0
Lucascserrate <lucascserrate@gmail.com>
2023-03-07 20:50:46 -0400

    creando los componentes e instalando tailwindcss

--- client/src/Components/LandingPage.jsx
+++ client/src/Components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Form = () => {
+const LandingPage = () => {
     return (
         <div>
 
@@ -8,4 +8,4 @@ const Form = () => {
     );
 };
 
-export default Form;
+export default LandingPage;
