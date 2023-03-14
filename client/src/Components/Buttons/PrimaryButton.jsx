68f224b314227dd31a08d21a45d71d1f08db5a2c
Lucas Camacho <Lucascserrate@gmail.com>
2023-03-14 19:53:09 -0400

    importando el navigate

--- client/src/Components/Buttons/PrimaryButton.jsx
+++ client/src/Components/Buttons/PrimaryButton.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PrimaryButton = ({ name, onClick, route }) => {
+    const navigate = useNavigate()
     const handleClick = () => {
-
+        if (route) {
+            navigate(route)
+        } else {
+            onClick()
+        }
     }
     return (
         <button className='py-2 px-6 bg-white bg-opacity-5 text-white rounded-md hover:bg-white hover:text-black font-bold' onClick={handleClick}  >
