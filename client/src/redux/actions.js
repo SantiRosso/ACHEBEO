9457ba653aea3eccda2b438a15707a525c41ce3f
Lucas Camacho <Lucascserrate@gmail.com>
2023-03-16 01:00:00 -0400

    adding redux config

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const GET_MOVIES = 'GET_MOVIES'
 
 export const getMovies = () => async dispatch => {
-    let json = await axios.get('/movies')
+    let json = await axios.get('/http://localhost:3001/movies')
     try {
         return dispatch({ type: GET_MOVIES, payload: json.data })
     } catch (error) {
