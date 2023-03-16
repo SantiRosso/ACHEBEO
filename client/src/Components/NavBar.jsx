436bb72ecdb830d772d021f07f88686a7f629a46
Lucas Camacho <Lucascserrate@gmail.com>
2023-03-15 23:57:18 -0400

    Little change to NavBar

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -4,18 +4,18 @@ import { AiOutlineMenu } from 'react-icons/ai'
 
 const NavBar = () => {
     const navigate = useNavigate()
-    return(
+    return (
         // <div className="flex justify-between items-center bg-gray-900 opacity-50 h-14">
-        <div className="flex justify-between items-center h-14">
+        <div className="flex justify-between items-center h-14 mx-16 my-4">
             <div className="flex justify-between items-center">
-                <AiOutlineMenu size='20px' color="white" className="cursor-pointer mx-5"/>
-                <p className="text-white cursor-pointer mx-5 font-bold" onClick={()=> navigate("/")}>Peliculas</p>
-                <p className="text-white cursor-pointer mx-5 font-bold" onClick={()=> navigate("/")}>Series</p>
+                <AiOutlineMenu size='20px' color="white" className="cursor-pointer" />
+                <p className="text-white cursor-pointer mx-5 font-bold" onClick={() => navigate("/")}>Peliculas</p>
+                <p className="text-white cursor-pointer mx-5 font-bold" onClick={() => navigate("/")}>Series</p>
             </div>
             <p className="text-white cursor-pointer text-xl font-bold -translate-x-20">MovieLabs</p>
             <div className="flex justify-between items-center">
-                <BiSearch size='20px' color='white' className="cursor-pointer" mx-5 onClick={()=> navigate("/")} />
-                <p className="text-white cursor-pointer mx-5 font-bold" onClick={()=> navigate("/")}>Perfil</p>
+                <BiSearch size='20px' color='white' className="cursor-pointer" mx-5 onClick={() => navigate("/")} />
+                <p className="text-white cursor-pointer mx-5 font-bold" onClick={() => navigate("/")}>Perfil</p>
                 {/* Iniciar sesión 
                 Suscríbete */}
             </div>
